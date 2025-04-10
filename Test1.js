4f0793448574dd14ed33c8d2c82731dc2340c611
tuha11598 <108464934+tuha11598@users.noreply.github.com>
2025-04-10 09:24:21 +0700

    Commit code

---
+++ Test1.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Sample JavaScript</title>
+</head>
+<body>
+  <h1>Hello, JavaScript!</h1>
+  <button onclick="showMessage()">Click Me</button>
+
+  <script>
+    function showMessage() {
+      alert("Button was clicked!");
+    }
+  </script>
+</body>
+</html>
